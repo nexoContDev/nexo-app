82dbb2d1adae181fdb31a130b2613f9ab533b0d0
nexoContDev <nexocontabeis@gmail.com>
2026-05-12 16:07:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import AdminApp from "./Admin.jsx"
+
+// Roteamento simples por URL
+const isAdmin = window.location.pathname.startsWith("/admin");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isAdmin ? <AdminApp /> : <App />}
   </React.StrictMode>
 )
